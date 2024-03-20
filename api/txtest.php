0703e45f1458713ac7d8f1ca4cbcf6cb66711f14
LeiCraft <linusfisscher08@gmx.net>
2024-03-20 12:12:34 +0100

    update

--- api/txtest.php
+++ api/txtest.php
@@ -8,27 +8,10 @@ header('Content-Type: application/json; charset=utf-8');
 
 $postData = json_decode(file_get_contents('php://input'), true);
 
-$requiredPostDataList = array(
-    "senderAddress",
-    "publicKey",
-    "privateKey",
-    "recipientAddress",
-    "amount"
-);
-
-
 $decoded_private_key = base64_decode($postData["privateKey"]);
 
 // Sign the transaction with the private key
 openssl_sign("abc" , $signature, $decoded_private_key, OPENSSL_ALGO_SHA256);
-
-// Convert the signature to a base64-encoded string and add it to the transaction data
-$transactionData['signature'] = base64_encode($signature);
-
-// generate the txid and add it to the transaction data
-$txid = hash('sha256', getPreparedObjectForHashing($transactionData));
-$transactionData = ['txid' => $txid] + $transactionData;
-
 
 
 http_response_code(200);
