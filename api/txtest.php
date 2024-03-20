51ee91cd95acc8bd2f3b1c8e00fbd6bbe1f19b0a
LeiCraft <linusfisscher08@gmx.net>
2024-03-20 12:05:00 +0100

    update

--- api/txtest.php
+++ api/txtest.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json; charset=utf-8');
 
 $postData = json_decode(file_get_contents('php://input'), true);
