d3809c24958c74a6725f685b2c3610e62ce3b9bd
LeiCraft <linusfisscher08@gmx.net>
2024-08-08 10:05:09 +0200

    added bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-const app = createApp(App)
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index.js';
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount('#app');
+
