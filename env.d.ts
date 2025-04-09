9cba235f967282c2d9cc47df0349be357318e3c0
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-04-09 10:07:32 +0100

    switched from vue to nuxt

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,14 @@
-/// <reference types="vite/client" />
+/// <reference types="@cloudflare/workers-types/2023-07-01" />
+
+declare module "h3" {
+  interface H3EventContext {
+    cf: CfProperties;
+    cloudflare: {
+      request: Request;
+      env: Env;
+      context: ExecutionContext;
+    };
+  }
+}
+
+export {};
