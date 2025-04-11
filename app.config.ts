61139145a17fcd7f924c94f05276645035ac131d
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-04-11 22:19:58 +0200

    update

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,6 @@
 export default defineAppConfig({
-    title: 'Leicoin - Decentralized, fast and secure blockchain',
-    theme: {
-      dark: true
-    }
-  })
-  
+	title: 'Leicoin - Decentralized, fast and secure blockchain',
+	theme: {
+		dark: true
+	}
+})
